eb2c8b45e0225c72c7d33e509da3b6525d6c921d
James Wade <jpswade@gmail.com>
2017-08-20 21:36:12 +0100

    Fix the liquid error

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 cd..
+brew install ruby
+sudo gem update
 bundle>/dev/null || sudo gem install bundler
 brew install libxml2
 brew link --force libxml2
@@ -7,3 +9,6 @@ sudo gem update --system
 sudo gem install pkg-config -v "~> 1.1.7"
 sudo env ARCHFLAGS="-arch x86_64" gem install -n /usr/local/bin nokogiri -v '1.6.8.1' -- --use-system-libraries --with-xml=/usr/local/Cellar/libxml2/2.9.4_3/
 bundle install
+sudo gem install bundler
+gem install nokogiri -v '1.8.0'
+bundle update
